ee246c0a95f3ad5b78262a0c77a6630d716c9b0a
Luke <logueluke41@gmail.com>
2023-03-02 01:03:06 +0000

    added more info for local/virtual enviroments.

--- gbq_query.py
+++ gbq_query.py
@@ -2,15 +2,18 @@ import os
 from google.cloud import bigquery
 import pandas as pd
 
+#Below are steps to run this file locally. GOOGLE_APPLICATION_CREDENTIALS are not needed when running from cloud shell editor or other Google enviroments.
 #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'ENTER_CREDENTIAL_PATH_HERE'
 #To create a new key go here https://cloud.google.com/bigquery/docs/quickstarts/quickstart-client-libraries#client-libraries-install-python
+#client = bigquery.Client()
 
-client = bigquery.Client()
+#Fill in LOCATION_OF_DATASET and GBQ_PROJECT below if using in google enviroment.
+client = bigquery.Client(location = 'LOCATION_OF_DATASET', project='GBQ_PROJECT')
 
 # Perform a query.
 QUERY = (
     '''
---Insert Query here.
+--Paste query here.
     ''')
 query_job = client.query(QUERY)  # API request
 df = query_job.to_dataframe() # Waits for query to finish
