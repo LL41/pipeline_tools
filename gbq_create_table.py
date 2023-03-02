ec9555bbc4672718d6f386388e7977a965c77673
Luke <logueluke41@gmail.com>
2023-03-02 05:37:34 +0000

    Cleared dataframe.

--- gbq_create_table.py
+++ gbq_create_table.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 import pandas_gbq
 
 #Replace PROJECT
@@ -7,15 +7,6 @@ project_id = "PROJECT"
 #Replace DATASET.TABLE
 table_id = 'DATASET.TABLE'
 
-df = pandas.DataFrame(
-    {
-        "my_string": ["a", "b", "c"],
-        "my_int64": [1, 2, 3],
-        "my_float64": [4.0, 5.0, 6.0],
-        "my_bool1": [True, False, True],
-        "my_bool2": [False, True, False],
-        "my_dates": pandas.date_range("now", periods=3),
-    }
-)
+df = pd.DataFrame()
 
 pandas_gbq.to_gbq(df, table_id, project_id=project_id)
